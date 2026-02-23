85e279a961789079fd29a9ec2876944770db962b
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-02-22 23:16:06 -0800

    added confirmation and admin page

--- app.js
+++ app.js
@@ -12,6 +12,32 @@ app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/index.html`);
 });
 
+const Contacts = [];
+
+app.post("/confirmation", (req, res) => {
+  const Contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    job: req.body.job,
+    company: req.body.company,
+    linkedin: req.body.linkedin,
+    email: req.body.linkedin,
+    met: req.body.meet,
+    other: req.body.other,
+    message: req.body.message,
+    mailing: req.body.mailing,
+    format: req.body.format,
+    timestamp: new Date(),
+  };
+
+  Contacts.push(Contact);
+  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+app.get("/admin", (req, res) => {
+  res.send(Contacts);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
