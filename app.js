81b0e30635525faeda104c7787e7ba95902f6ed8
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-03-18 11:16:44 -0700

    database works now

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -16,28 +20,61 @@ app.get("/", (req, res) => {
 
 const Contacts = [];
 
-app.post("/confirmation", (req, res) => {
-  const Contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    job: req.body.job,
-    company: req.body.company,
-    linkedin: req.body.linkedin,
-    email: req.body.email,
-    meet: req.body.meet,
-    other: req.body.other,
-    message: req.body.message,
-    mailing: req.body.mailing,
-    format: req.body.format,
-    timestamp: new Date(),
-  };
+const pool = mysql2
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  })
+  .promise();
 
-  Contacts.push(Contact);
-  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+app.post("/confirmation", async (req, res) => {
+  const params = [
+    req.body.firstName,
+    req.body.lastName,
+    req.body.job,
+    req.body.company,
+    req.body.linkedin,
+    req.body.email,
+    req.body.meet,
+    req.body.other,
+    req.body.message,
+    req.body.mailing,
+    req.body.format,
+    new Date(),
+  ];
+
+  const sql = ` INSERT INTO contacts (fname, lname, job, company, linkedin, email, meet, other, message, mailing, format, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  // const Contact = {
+  //   firstName: req.body.firstName,
+  //   lastName: req.body.lastName,
+  //   job: req.body.job,
+  //   company: req.body.company,
+  //   linkedin: req.body.linkedin,
+  //   email: req.body.email,
+  //   meet: req.body.meet,
+  //   other: req.body.other,
+  //   message: req.body.message,
+  //   mailing: req.body.mailing,
+  //   format: req.body.format,
+  //   timestamp: new Date(),
+  // };
+
+  // Contacts.push(Contact);
+
+  const result = await pool.execute(sql, params);
+  res.render("confirmation", { Contact: req.body });
 });
 
-app.get("/admin", (req, res) => {
-  res.render("admin", { Contacts });
+app.get("/admin", async (req, res) => {
+  const contacts = await pool.query(
+    "Select * FROM contacts ORDER BY timestamp DESC",
+  );
+  console.log(contacts);
+  res.render("admin", { Contacts: contacts[0] });
 });
 
 app.get("/contact", (req, res) => {
